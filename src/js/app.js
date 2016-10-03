25206e81d51f407ec597125d029daed3ae637a18
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-10-03 17:30:12 +0800

    added add-guest-to-list functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,33 @@
-var signUp = $('#sign-up-button');
+var Event = function() {
+	this.name = ko.observable();
+	this.location = ko.observable();
+	this.type = ko.observable();
+	this.host = ko.observable();
+	this.startDate = ko.observable();
+	this.startTime = ko.observable();
+	this.endDate = ko.observable();
+	this.endTime = ko.observable();
+	this.guests = ko.observableArray([]);
+};
 
-signUp.click(function() {
-	window.location.href = 'event.html';
-});
+var ViewModel = function() {
+	var self = this;
+
+	self.eventList = ko.observableArray([]);
+	self.currentGuestList = ko.observableArray([]);
+
+	self.addGuestToList = function(data, event) {
+		if(event.keyCode === 13) {
+			self.currentGuestList.push({name: event.target.value});
+			event.target.value = "";
+		}
+		return true;
+	};
+};
+
+// initialize ViewModel and apply bindings
+var model = new ViewModel();
+ko.applyBindings(model);
 
 var placeSearch, autocomplete;
 
@@ -31,3 +56,22 @@ function geolocate() {
   		});
 	}
 }
+
+$('#saveEventBtn').click(function() {
+	var event = new Event();
+	event.name = $('#eventName').val();
+	event.location = $('#eventLocation').val();
+	event.type = $('#eventType').val();
+	event.host = $('#eventHost').val();
+	event.startDate = $('#startDateTime').val();
+	event.startTime = $('#startTime').val();
+	event.endDate = $('#endDateTime').val();
+	event.endTime = $('#endTime').val();
+});
+
+// index.html page
+var signUp = $('#sign-up-button');
+
+signUp.click(function() {
+	window.location.href = 'event.html';
+});
