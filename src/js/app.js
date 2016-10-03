18c1256ca727881eb4d2cda8037233dbdb694110
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-10-03 14:46:24 +0800

    added autocomplete functionality for event location

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,31 @@ var signUp = $('#sign-up-button');
 signUp.click(function() {
 	window.location.href = 'event.html';
 });
+
+var placeSearch, autocomplete;
+
+function initAutocomplete() {
+	// Create the autocomplete object, restricting the search to geographical
+	// location types.
+	autocomplete = new google.maps.places.Autocomplete(
+    	/** @type {!HTMLInputElement} */(document.getElementById('eventLocation')),
+    	{types: ['geocode']});
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+	if (navigator.geolocation) {
+  		navigator.geolocation.getCurrentPosition(function(position) {
+    		var geolocation = {
+      			lat: position.coords.latitude,
+      			lng: position.coords.longitude
+    		};
+    		var circle = new google.maps.Circle({
+      			center: geolocation,
+     			radius: position.coords.accuracy
+    		});
+    		autocomplete.setBounds(circle.getBounds());
+  		});
+	}
+}
